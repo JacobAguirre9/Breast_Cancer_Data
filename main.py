3f93f490ebff7f71d1a153ebb9bb4821af985376
JacobAguirre9 <jaguirre31@gatech.edu>
2022-04-05 22:42:15 -0400

    updated

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-#This is code for Breast Cancer prediction on the commonly used University of Wisconsin-Madison Breast cancer dataset.
+# This is code for Breast Cancer prediction on the commonly used University of Wisconsin-Madison Breast cancer dataset.
 
+# Main Objective-- Develop accurate and precise algorithm to classify breast cancer detection
 
 print("Hello World")
+# importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+# importing our cancer dataset
+dataset = pd.read_csv(breast-cancer-wisconsin.csv)
+X = dataset.iloc[:, 1:31].values
+Y = dataset.iloc[:, 31].values
 
-print("Bye World")
+dataset.head()
+
+
+print("Goodbye World")
