d059c5e95d12f7b3cefe3c29b7e7281d7297a145
JacobAguirre9 <jaguirre31@gatech.edu>
2022-04-06 00:18:28 -0400

    updated

--- main.py
+++ main.py
@@ -72,4 +72,101 @@ fig = px.imshow(df[cols].corr());
 fig.show()
 # </editor-fold>
 
+# <editor-fold desc="Model Implementation Code">
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, f1_score
+from sklearn.metrics import classification_report
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_validate, cross_val_score
+from sklearn.svm import SVC
+from sklearn import metrics
+
+prediction_feature = ["radius_mean",  'perimeter_mean', 'area_mean', 'symmetry_mean', 'compactness_mean', 'concavepoints_mean']
+
+targeted_feature = 'diagnosis'
+
+len(prediction_feature)
+X = df[prediction_feature]
+X
+print(X.shape)
+print(X.values)
+y = df.diagnosis
+y
+print(y.values)
+
+# Now, split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=15)
+print(X_train)
+print(X_test)
+
+# Scale the data to keep all the values in the same magnitude of 0 -1
+
+sc = StandardScaler()
+
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
+
+
+# </editor-fold>
+
+# <editor-fold desc="ML model selection and model prediction">
+def model_building(model, X_train, X_test, y_train, y_test):
+       """
+
+       Model Fitting, Prediction And Other stuff
+       return ('score', 'accuracy_score', 'predictions' )
+       """
+
+       model.fit(X_train, y_train)
+       score = model.score(X_train, y_train)
+       predictions = model.predict(X_test)
+       accuracy = accuracy_score(predictions, y_test)
+
+       return (score, accuracy, predictions)
+
+models_list = {
+    "LogisticRegression" :  LogisticRegression(),
+    "RandomForestClassifier" :  RandomForestClassifier(n_estimators=10, criterion='entropy', random_state=5),
+    "DecisionTreeClassifier" :  DecisionTreeClassifier(criterion='entropy', random_state=0),
+    "SVC" :  SVC(),
+}
+
+print(models_list)
+print(list(models_list.keys()))
+print(list(models_list.values()))
+
+print(zip(list(models_list.keys()), list(models_list.values())))
+
+
+def cm_metrix_graph(cm):
+       sns.heatmap(cm, annot=True, fmt="d")
+       plt.show()
+
+
+df_prediction = []
+confusion_matrixs = []
+df_prediction_cols = ['model_name', 'score', 'accuracy_score', "accuracy_percentage"]
+
+for name, model in zip(list(models_list.keys()), list(models_list.values())):
+       (score, accuracy, predictions) = model_building(model, X_train, X_test, y_train, y_test)
+
+       print("\n\nClassification Report of '" + str(name), "'\n")
+
+       print(classification_report(y_test, predictions))
+
+       df_prediction.append([name, score, accuracy, "{0:.2%}".format(accuracy)])
+
+       # For Showing Metrics
+       confusion_matrixs.append(confusion_matrix(y_test, predictions))
+
+df_pred = pd.DataFrame(df_prediction, columns=df_prediction_cols)
+
+# </editor-fold>
+
 print("Goodbye World")
