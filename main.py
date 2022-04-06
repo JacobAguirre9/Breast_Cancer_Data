45d57f79c9e0d928db8651ce625a85884b45e51a
JacobAguirre9 <jaguirre31@gatech.edu>
2022-04-06 00:07:10 -0400

    updated

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ sns.pairplot(df[cols], hue="diagnosis")
 plt.show()
 # </editor-fold>
 
+# <editor-fold desc="Label Creation">
 from sklearn.preprocessing import LabelEncoder
 # LabelEncoder can be used to normalize labels.
 print(df.head(2))
@@ -47,8 +48,28 @@ labelencoder_Y = LabelEncoder()
 df.diagnosis = labelencoder_Y.fit_transform(df.diagnosis)
 print(df.head(2))
 
-print(data.diagnosis.value_counts())
-print("\n", data.diagnosis.value_counts().sum())
+print(df.diagnosis.value_counts())
+print("\n", df.diagnosis.value_counts().sum())
+# </editor-fold>
 
+# <editor-fold desc="Correlation Matrix">
+cols = ['diagnosis', 'radius_mean', 'texture_mean', 'perimeter_mean',
+       'area_mean', 'smoothness_mean', 'compactness_mean', 'concativity_mean',
+       'concavepoints_mean', 'symmetry_mean', 'fractal_dimension_mean']
+print(len(cols))
+corrmatrix = df[cols].corr()
+print(corrmatrix)
+plt.figure(figsize=(12, 9))
+
+plt.title("Correlation Graph")
+
+cmap = sns.diverging_palette( 1000, 120, as_cmap=True)
+sns.heatmap(df[cols].corr(), annot=True, fmt='.1%',  linewidths=.05, cmap=cmap);
+plt.figure(figsize=(15, 10))
+
+
+fig = px.imshow(df[cols].corr());
+fig.show()
+# </editor-fold>
 
 print("Goodbye World")
